66d5bbe16245dfbf35d19c45d6e10f767b75c11a
Michael Riehle <aikimcr@gmail.com>
2022-09-11 16:40:03 -0700

    Loader is allowing login.

--- src/Network/Loader.js
+++ src/Network/Loader.js
@@ -1,48 +1,12 @@
 // Load the data from the server.  Handle all the boilerplate for xhr
 // correctly.
 
-export const listUrl = 'https://api.coingecko.com/api/v3/exchanges';
-export const perPage = 10;
+import { getFromPath, postToPath } from './Network.js';
 
-export function load(url, query = {}) {
-  if (process.env.NODE_ENV === 'test') {
-    debugger;
-    return Promise.resolve([]);
-  }
+export function loginStatus()  {
+  return getFromPath('login');
+};
 
-  const requestUrl = new URL(url);
-
-  for (const param in query) {
-    requestUrl.searchParams.set(param, query[param]);
-  }
-
-  return new Promise((resolve, reject) => {
-    var xhr = new XMLHttpRequest();
-    xhr.responseType = 'json';
-    xhr.open('GET', requestUrl);
-    xhr.onload = function() {
-      if (xhr.status === 200) {
-        resolve(xhr.response);
-      }
-      else {
-        reject({status: xhr.status, message: xhr.responseText});
-      }
-    };
-    xhr.send();
-  });
+export function login(username, password) {
+  return postToPath('login', {username, password});
 }
-
-export async function loadList(page) {
-  const result = await load(listUrl, {per_page: 10, page: page});
-  // This is going to need caching at some point.
-  return result;
-}
-
-export async function loadDetail(exchangeId) {
-  const detailUrl = `${listUrl}/${exchangeId}`;
-  const result = await load(detailUrl, {});
-  // Again, caching is really needed here.
-  return result;
-}
-
-export default load;
